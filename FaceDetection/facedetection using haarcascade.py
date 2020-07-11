de52aec202b0c45f322c0c220c1b285ee4110554
Rayman174-A <rayman1128.cse18@chitkara.edu.in>
2020-07-11 18:44:34 +0530

    Update facedetection using haarcascade.py

--- FaceDetection/facedetection using haarcascade.py
+++ FaceDetection/facedetection using haarcascade.py
@@ -1,36 +1,30 @@
+# Read a video stream from camera(frame by frame)
 import cv2
 
-cap=cv2.VideoCapture(0)
-face_cascade=cv2.CascadeClassifier("haarcascade_frontalface.xml")
+cap=cv2.VideoCapture(0)# default webcam
+face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 
 while True:
+	ret,frame=cap.read()#ret is a boolean value representing capture of image
+	gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
-   ret,frame=cap.read()
-   gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+	
+    
+	if ret==False:
+		continue
+	faces=face_cascade.detectMultiScale(frame,1.3,5)
 
-   if ret==False:
-   	continue;
-
-   faces=face_cascade.detectMultiScale(gray_frame,1.3,5)
-
-   
-   #cv2.imshow("Gray Frame",gray_frame)
-
-   for (x,y,w,h) in faces:
-
-   	cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)#color and boundary are last two params.
-   
-   cv2.imshow("Video Frame",frame)
-   
-
-   #Wait for the user input -q, then you will stop the loop
-
-   key_pressed=cv2.waitKey(1) & 0xFF
-   if key_pressed ==ord('q'):
-   	break;
+	for (x,y,w,h) in faces:
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(125,0,130),2)
 
 
-   
+	cv2.imshow("Video Frame",frame)
+	#cv2.imshow("Gray Frame",gray_frame)
+
+	#Wait for user input. Like user presses q and then you will stop
+	key_pressed=cv2.waitKey(1) & 0xFF ##0xFF has 8 ones(converts 32 bit into 8 bit)
+	if key_pressed ==ord('q'):#Ord return ASCII Value
+		break
 
 cap.release()
 cv2.destroyAllWindows()
